0ffa4379fa3dc6688cf8d5756644ebd13b20b579
samayisrael <samayisrael@users.noreply.github.com>
2021-05-25 13:10:35 -0700

    added email test

--- my_library/memoization.py
+++ my_library/memoization.py
@@ -26,3 +26,43 @@ class Fibber(object):
         self.memo[n] = result
 
         return result
+
+
+
+class Stack(object):
+
+    def __init__(self):
+        """Initialize an empty stack"""
+        self.items = []
+
+    def push(self, item):
+        """Push a new item onto the stack"""
+        self.items.append(item)
+
+    def pop(self):
+        """Remove and return the last item"""
+        # If the stack is empty, return None
+        # (it would also be reasonable to throw an exception)
+        if not self.items:
+            return None
+
+        return self.items.pop()
+
+    def peek(self):
+        """Return the last item without removing it"""
+        if not self.items:
+            return None
+        return self.items[-1]
+
+class MaxStack(object):
+
+    def __init__(self):
+        self.max_element = 0
+
+    def get_max(self):
+        self.max_element =
+
+
+myStack = Stack()
+myStack.push()
+print(has_cycle)
